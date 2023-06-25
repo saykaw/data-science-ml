44d98c0ade235935b597926636aee2797e16d72a
Sayali <saykaw16@gmail.com>
2023-06-25 16:05:09 +0530

    gradient descent

---
+++ linear_regression/from_scratch.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+def linearregression(x,y):
+    # we have to find the value to m and b for these data points
+    # y=mx+b
+    steps = 1000
+    step_size = 0.05 #learning rate
+    m_current = b_current = 0
+    n=len(x)
+    i=0
+    for i in range(steps):
+        y_pred = m_current * x + b_current
+        md = -(2/n)* sum(x*(y-y_pred))
+        bd = -(2/n)* sum((y-y_pred))
+        m_current = m_current - step_size * md
+        b_current = b_current - step_size * bd
+        print("m:",m_current," b:",b_current," steps:",i+1)
+
+
+x_arr = np.array([1,2,3,4,5])
+y_arr = np.array([5,7,9,11,13])
+linearregression(x_arr,y_arr)  
+
+
+print("====comparing with sklearn====")
+import sklearn 
+from sklearn.linear_model import LinearRegression
+import pandas as pd 
+df = pd.read_csv("sample.csv")
+model = LinearRegression()
+model.fit(df[['x']],df['y'])
+a = model.coef_
+b = model.intercept_
+print("slope:",a)
+print("intercept:",b)
+
+
+
+    
+
+
+
+    
+
